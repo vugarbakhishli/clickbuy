7cfd5ce4daa4a89825658ea7d33021228621cd29
Vugar Bakhishli <vugarbakhishli@pop-os.localdomain>
2021-07-30 22:39:23 +0400

    Initial commit

---
+++ src/main/java/com/bakhishli/clickbuy/ClickbuyApplication.java
@@ -0,0 +1,13 @@
+package com.bakhishli.clickbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakhishli/clickbuy/ClickbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakhishli.clickbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
